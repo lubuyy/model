1a6766907df387d7a659bf0c7c92502526fdad2c
lubuyy <2313378167@qq.com>
2020-04-19 18:02:17 +0800

    app

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Tv',
+    component: () => import('../views/tv/Tv.vue')
+  },{
+    path: '/book',
+    name: 'Book',
+    component: () => import('../views/book/Book.vue'),
+    // beforeEnter: (to, from, next) => {
+    //   console.log(to,from,next)
+    // }
+  },{
+    path: '/music',
+    name: 'Music',
+    component: () => import('../views/music/Music.vue')
+  },{
+    path: '/talk',
+    name: 'Talk',
+    component: () => import('../views/talk/Talk.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
